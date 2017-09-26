c69de0ef7c65ee6ff32bc4f07dd35f3f5e08047b
Nikita Sharma <nikitasharma9010@gmail.com>
2017-09-26 11:40:42 +0530

    Code reduction, Replace comments  with docstrings

--- log_analysis_db.py
+++ log_analysis_db.py
@@ -5,15 +5,13 @@ import psycopg2
 DBNAME = "news"
 
 
-# Q1. What are the most popular three articles of all time?
-# i.e. Which articles have been accessed the most?
-def top3_articles():
+def exceute_query(query):
+    """Method to execute sql queries"""
+
     # Connect to database
     db = psycopg2.connect(database=DBNAME)
     # Create cursor object
     c = db.cursor()
-    # Create query
-    query = "SELECT title, article_view_count FROM article_views LIMIT 3;"
     # Execute query
     c.execute(query)
     # Fetch data
@@ -24,14 +22,17 @@ def top3_articles():
     return logs
 
 
-# Q2. Who are the most popular article authors of all time?
-# i.e. When you sum up all of the articles each author has written,
-# which authors get the most page views?
+def top3_articles():
+    """To find the most popular three articles of all time"""
+
+    # Create query
+    query = "SELECT title, article_view_count FROM article_views LIMIT 3;"
+    return exceute_query(query)
+
+
 def top_authors():
-    # Connect to database
-    db = psycopg2.connect(database=DBNAME)
-    # Create cursor object
-    c = db.cursor()
+    """To find the most popular article authors of all time"""
+
     # Create query
     query = """SELECT authors.name,
     SUM(article_views.article_view_count) AS author_view_count
@@ -39,32 +40,15 @@ def top_authors():
     WHERE article_views.author=authors.id
     GROUP BY authors.id
     ORDER BY author_view_count DESC;"""
-    # Execute query
-    c.execute(query)
-    # Fetch data
-    logs = c.fetchall()
-    # Close database connection
-    db.close()
-    # Return data
-    return logs
+    return exceute_query(query)
 
 
-# Q3. On which days did more than 1% of requests lead to errors?
 def error_gt_1percent():
-    # Connect to database
-    db = psycopg2.connect(database=DBNAME)
-    # Create cursor object
-    c = db.cursor()
+    """To find days On which more than 1% of requests lead to errors"""
+
     # Create query
     query = """SELECT day, error, total_requests
     FROM daywise_status_log
     WHERE error > (0.01 * total_requests)
     ORDER BY day;"""
-    # Execute query
-    c.execute(query)
-    # Fetch data
-    logs = c.fetchall()
-    # Close database connection
-    db.close()
-    # Return data
-    return logs
+    return exceute_query(query)
